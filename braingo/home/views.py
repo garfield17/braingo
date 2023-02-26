cf92c9de2cc19fdff42a4b99e8c188ff60f73dda
garfield17 <zsaliy2003@mail.ru>
2023-02-26 21:34:45 +0300

    apps

---
+++ braingo/home/views.py
@@ -0,0 +1,10 @@
+import os
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'home/index.html')
