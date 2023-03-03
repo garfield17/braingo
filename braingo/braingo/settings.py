3f6bb8c364cabf6cd4a64af5eaa1e24223d5d9bb
garfield17 <zsaliy2003@mail.ru>
2023-03-03 12:35:13 +0300

    testing

--- braingo/braingo/settings.py
+++ braingo/braingo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'register',
     'subject',
     'topic',
+    'testing',
+    'tests'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- braingo/subject/models.py
+++ braingo/subject/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Subjects(models.Model):
+    name = models.CharField("Название", max_length=100, null=True)
+    description = models.TextField('Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
+        db_table = "subjects"
