79ddeab859e19a8ff22ad15c27bb7c5c164f7f88
garfield17 <zsaliy2003@mail.ru>
2023-03-03 11:20:04 +0300

    db accounts

--- braingo/account/models.py
+++ braingo/account/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Users(models.Model):
     name = models.CharField("Имя", max_length=10000, null=True)
     birthdate = models.DateField("Год рождения", null=True)
+    status = models.IntegerField("Статус активности", null=True, default=1)
     password = models.CharField("Пароль", max_length=10000, null=True)
     email = models.EmailField("Почта", max_length=10000, null=True)
     profile_picture = models.ImageField(upload_to='', null=True, blank=True, default="")
